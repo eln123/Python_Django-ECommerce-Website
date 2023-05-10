9e6d454e41e987ca53ac5040b0d4ebe83ede3087
Ethan <elnair122@gmail.com>
2023-05-10 13:46:09 -0400

    Postgres connected

--- storefront/app1/models.py
+++ storefront/app1/models.py
@@ -11,10 +11,11 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    slug = models.SlugField(default='a string')
 
 
 class Customer(models.Model):
--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from secrets import SECRET_KEY
+from secrets import SECRET_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront2',
+        'HOST': 'localhost',
+        'USER': 'owner',
+        'PASSWORD': SECRET_PASSWORD,
     }
 }
 
