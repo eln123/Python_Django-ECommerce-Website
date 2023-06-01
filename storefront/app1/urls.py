35f9ca38d4a4d1640a1951dda38b2e57c0b472fb
Ethan <elnair122@gmail.com>
2023-06-01 15:34:55 -0400

    base.html and navbar created and reusable/extends

--- storefront/app1/urls.py
+++ storefront/app1/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('products/', views.all_collections),
     path('products/<int:collection_id>/<int:id>', views.single_product),
     path('products/<int:collection_id>', views.single_collection),
+    path('orders/', views.all_orders),
 ]
